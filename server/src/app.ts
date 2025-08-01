c31ac395dd888c100cfd250826e9ef96c40c774a
mahipal kachhad <mahipalkac@gmail.com>
2025-08-01 17:21:00 +0530

    final fix for cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://newslyus.com",
+    origin: "https://chatapp-one-green.vercel.app/",
     credentials: true,
   })
 );
