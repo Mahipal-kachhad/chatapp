8c77ef7e852fafe1268f0d1ac4338d77b775cc4d
mahipal kachhad <mahipalkac@gmail.com>
2025-08-01 17:38:40 +0530

    final fix for cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-one-green.vercel.app/",
+    origin: "https://chatapp-one-green.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
